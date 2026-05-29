12eb823ea3063c06d73ce363c3d55e1cdd2b3c87
Krati Gaur <kratishreya6@gmail.com>
2026-05-29 23:50:14 +0530

    fix admin php syntax error

--- api/admin/media/upload.php
+++ api/admin/media/upload.php
@@ -6,7 +6,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: Content-Type, X-CSRF-Token');
 header('Access-Control-Allow-Methods: POST, OPTIONS');
 
-require_once __DIR__ . '/../../config/admin.php';
+require_once __DIR__ . '/../../../config/admin.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(204);
